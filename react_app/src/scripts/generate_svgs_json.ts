3bc5b6e28f7994aa2df5db6c859be454ae71c0f8
josevazf <jose.guilherme.rvf@gmail.com>
2024-05-07 14:41:04 +0100

    updated script to create svgs and metadata

--- react_app/src/scripts/generate_svgs_json.ts
+++ react_app/src/scripts/generate_svgs_json.ts
@@ -1,9 +1,10 @@
 // @ts-nocheck
 import axios from 'axios';
 import * as fs from 'fs';
+import { Readable } from 'stream';
 import { typeColors, typeSizes } from '../utils/typeRefs';
 
-var pokemonLimit = 151;
+var pokemonLimit = 10;
 
 // Gets all pokemons from API up to `pokemonLimit`
 async function getPokemons(): Promise<any> {
@@ -43,13 +44,29 @@ function breakText(text: string): [string, string] {
   return [firstPart, secondPart];
 }
 
+async function imageUrlToBase64(url: string): Promise<string> {
+  try {
+      // Fetch the image data
+      const response = await axios.get(url, { responseType: 'arraybuffer' });
+
+      // Convert the array buffer to base64
+      const base64String = Buffer.from(response.data, 'binary').toString('base64');
+
+      return base64String;
+  } catch (error) {
+      console.error('Error fetching or converting image:', error);
+      throw error;
+  }
+}
+
 // Updates the SVG Template with the Pokémon info
-async function updateSVGTemplate(
-  template: string,
+async function updateTemplates(
+  templateSVG: string,
+  templateMetadata: string,
   name: string,
   number: string,
   color: string,
-  image: string,
+  base64SVG: string,
   height: string,
   weight: string,
   ability1: string,
@@ -67,6 +84,7 @@ async function updateSVGTemplate(
   SATK_bar: string,
   SDEF_bar: string,
   SPD_bar: string,
+  description: string,
   description1: string,
   description2: string,
   type1txt: string,
@@ -78,11 +96,11 @@ async function updateSVGTemplate(
   type1color_x: string,
   type1txt_x: string
 ): Promise<string> {
-  const updatedTemplate = template
+  const updatedTempSVG = templateSVG
       .replace('{name}', name)
-      .replace('{number}', number)
+      .replace('{number}', `#${number}`)
       .replace(/{color}/g, color)
-      .replace('{image}', image)
+      .replace('{image}', base64SVG)
       .replace('{height}', height)
       .replace('{weight}', weight)
       .replace('{ability1}', ability1)
@@ -109,20 +127,40 @@ async function updateSVGTemplate(
       .replace('{type1w}', type1w)
       .replace('{type2w}', type2w)
       .replace('{type1color_x}', type1color_x)
-      .replace('{type1txt_x}', type1txt_x)
-  return updatedTemplate;
+      .replace('{type1txt_x}', type1txt_x);
+  
+      const updatedTempMetadata = templateMetadata
+      .replace('{name}', name)
+      .replace('"{number}"', Number(number))
+      .replace('"{height}"', Number(height))
+      .replace('"{weight}"', Number(weight))
+      .replace('{ability1}', ability1)
+      .replace('{ability2}', ability2)
+      .replace('"{HP}"', Number(HP))
+      .replace('"{ATK}"', Number(ATK))
+      .replace('"{DEF}"', Number(DEF))
+      .replace('"{SATK}"', Number(SATK))
+      .replace('"{SDEF}"', Number(SDEF))
+      .replace('"{SPD}"', Number(SPD))
+      .replace('{description}', description)
+      .replace('{type1txt}', type1txt)
+      .replace('{type2txt}', type2txt)
+
+  return [updatedTempSVG, updatedTempMetadata];
 }
 
 async function main() {
   try {
-    const template = fs.readFileSync('./poke_template.svg', 'utf-8');
+    const templateSVG = fs.readFileSync('./svg_template.svg', 'utf-8');
+    const templateMetadata = fs.readFileSync('./metadata_template.json', 'utf-8');
     const [pokemonData, pokemonSpeciesData] = await getPokemons();
+    const fileWritePromises: Promise<void>[] = [];
 
     for (let i = 0; i < pokemonData.length; i++ ) {
       const name = pokemonData[i].data.name.charAt(0).toUpperCase() + pokemonData[i].data.name.slice(1);
       const id = pokemonData[i].data.id;
       const color = typeColors[pokemonData[i].data.types[0].type.name.toLowerCase()];
-      const number = `#${String(id).padStart(3, '0')}`;
+      const number = `${String(id).padStart(3, '0')}`;
       const image = pokemonData[i].data.sprites.other['official-artwork'].front_default;
       const height = pokemonData[i].data.height;
       const weight = pokemonData[i].data.weight;
@@ -140,7 +178,10 @@ async function main() {
       const SATK = pokemonData[i].data.stats[3].base_stat;
       const SDEF = pokemonData[i].data.stats[4].base_stat;
       const SPD = pokemonData[i].data.stats[5].base_stat;
-      const description = pokemonSpeciesData[i].data.flavor_text_entries[1].flavor_text.replace(/\u000C/g, ' ').replace(/POKéMON/g, "Pokémon");
+      const description = pokemonSpeciesData[i].data.flavor_text_entries[1].flavor_text
+        .replace(/\u000C/g, ' ')
+        .replace(/POKéMON/g, "Pokémon")
+        .replace(/\n/g, ' ');
       const [description1, description2] = breakText(description);
       const type1txt = pokemonData[i].data.types[0].type.name.toLowerCase();
       const type2txt = pokemonData[i].data.types[1] ? pokemonData[i].data.types[1].type.name.toLowerCase(): "";
@@ -151,12 +192,16 @@ async function main() {
       const type1color_x = pokemonData[i].data.types[1] ? 172 - type1w : 180 - (type1w / 2);
       const type1txt_x = pokemonData[i].data.types[1] ? 172 - type1w + 8 : type1color_x + 8;
 
-      const updatedTemplate = await updateSVGTemplate(
-        template,
+      // Call the function to convert the image URL to base64
+      const base64SVG = await imageUrlToBase64(image);
+
+      const [updatedTempSVG, updatedTempMetadata] = await updateTemplates(
+        templateSVG,
+        templateMetadata,
         name,
         number,
         color,
-        image,
+        base64SVG,
         height,
         weight,
         ability1,
@@ -174,10 +219,11 @@ async function main() {
         String(parseInt(SATK) / 255 * 233),
         String(parseInt(SDEF) / 255 * 233),
         String(parseInt(SPD) / 255 * 233),
+        description,
         description1,
         description2,
-        type1txt,
-        type2txt,
+        type1txt.charAt(0).toUpperCase() + type1txt.slice(1),
+        type2txt.charAt(0).toUpperCase() + type2txt.slice(1),
         type1color,
         type2color,        
         String(type1w),
@@ -186,12 +232,32 @@ async function main() {
         String(type1txt_x),
       );
 
-      // Create SVG file for each pokemon
-      fs.writeFileSync(`./out/poke_${String(number).padStart(3, '0')}.svg`, updatedTemplate);
-  }
+      const writeFilePromise = new Promise<void>((resolve, reject) => {
+        fs.writeFile(`./out/svg/${String(number)}.svg`, updatedTempSVG, (err) => {
+          if (err) {
+            reject(err);
+          } else {
+            fs.writeFile(`./out/json/${String(number)}.json`, updatedTempMetadata, (err) => {
+              if (err) {
+                reject(err);
+              } else {
+                resolve();
+              }
+            });
+          }
+        });
+      });
+      fileWritePromises.push(writeFilePromise);
+    }
+
+    console.log("⌛ Creating SVG files, please wait...\n")
+    // Wait for all file writing promises to resolve
+    await Promise.all(fileWritePromises);
+
+    console.log("✅ All SVG files have been successfully created.");
     //console.log("Pokemon data fetched:", data);
   } catch (error) {
-    console.error("Error fetching Pokemon data:", error);
+    console.error("❌ Error fetching Pokemon data:", error);
   }
 };
 
